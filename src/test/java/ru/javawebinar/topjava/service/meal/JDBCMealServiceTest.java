2252da11d69ca4bfc91386e03351de649b2b4387
Fireserdg <serdg1984@yandex.ru>
2019-04-07 17:25:55 +0300

    HW05_test_all_profiles

---
+++ src/test/java/ru/javawebinar/topjava/service/meal/JDBCMealServiceTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.service.meal;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.meal.AbstractMealServiceTest;
+
+
+@ActiveProfiles(Profiles.JDBC)
+public class JDBCMealServiceTest extends AbstractMealServiceTest {
+}
